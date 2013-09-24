6954b9f01804d256e7a9ea4efe5be7471b9140e4
kajookdev <kajookdev@kojosoft.org>
2013-09-24 13:23:37 +0700

    init

---
+++ example-winform-delegate/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        delegate void TextBoxDelegate(string message);
+        delegate void ProgressBarDelegate(int percentage);
+
+        public Form1()
+        {
+            InitializeComponent();
+            Form2 f2 = new Form2(this);
+            f2.MdiParent = this;
+            f2.Show();
+        }
+
+        public void updateText(string text)
+        {
+            if (this.statusStrip1.InvokeRequired)
+            {
+                this.statusStrip1.Invoke(new TextBoxDelegate(updateText), new object[] { text });
+            }
+            else
+            {
+                this.toolStripStatusLabel1.Text = text;
+            }
+        }
+
+        public void updateProgressbar(int percentage)
+        {
+            if (this.statusStrip1.InvokeRequired)
+            {
+                this.statusStrip1.Invoke(new ProgressBarDelegate(updateProgressbar), new object[] { percentage });
+
+            }
+            else
+            {
+                this.toolStripProgressBar1.Value = percentage;
+            }
+        }
+    }
+}
---
+++ example-winform-delegate/Form2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form2 : Form
+    {
+        BackgroundWorker bw = new BackgroundWorker();
+        private Form1 form1;
+        public Form2()
+        {
+            InitializeComponent();
+            bw.DoWork += new DoWorkEventHandler(bw_DoWork);
+            bw.ProgressChanged += new ProgressChangedEventHandler(bw_ProgressChanged);
+            bw.WorkerReportsProgress = true;
+        }
+
+        public Form2(Form1 form1):this()
+        {
+            this.form1 = form1;
+        }
+
+        void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.label1.Text = e.ProgressPercentage.ToString() ;
+            form1.updateText(e.ProgressPercentage.ToString());
+            form1.updateProgressbar(e.ProgressPercentage);
+        }
+
+        void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine("running " + i);
+                Thread.Sleep(1000);
+                bw.ReportProgress(i);
+                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!bw.IsBusy)
+            {
+                bw.RunWorkerAsync();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
